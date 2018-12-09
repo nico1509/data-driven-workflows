8fa8dd50ede4db6c8c4c7c3d2ada620073414072
Nico Aßfalg <nico.assfalg@student.kit.edu>
2018-12-09 16:34:37 +0100

    Change Workflow and ontology

--- draft/18-12-08/run.sh
+++ draft/18-12-08/run.sh
@@ -6,5 +6,5 @@ curl -f -X PUT -T workflow/ibm-workflow.ttl http://localhost/ldbbc/ -Hcontent-ty
 curl -f -X PUT -T ontology/ibm-vocab.ttl http://localhost/ldbbc/ -Hcontent-type:text/turtle || exit 3
 #curl -f -X PUT -T list-vocab.ttl http://localhost/ldbbc/ -Hcontent-type:text/turtle || exit 4
 
-JAVA_OPTS=-Dldfu.optimiser=OFF $ldf -p ontology/rules/data-retrieval-ldpc.n3 -p ontology/rules/ibm-semantics.n3 -p ontology/rules/spin_sparql_ask_where_query_processing.n3 -n 100 # fish-sync...
+JAVA_OPTS=-Dldfu.optimiser=OFF $ldf -p ontology/rules/data-retrieval-ldpc.n3 -p ontology/rules/ibm-semantics.n3 -p ontology/rules/spin_sparql_ask_where_query_processing.n3 -n 1500 #fish-sync...
 
