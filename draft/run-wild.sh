12def2c93d4a14cdb2e3325238fb399d09200489
Nico Assfalg <nico.assfalg@student.kit.edu>
2019-01-11 14:10:47 +0100

    UI mods. put this in master later

--- draft/run-wild.sh
+++ draft/run-wild.sh
@@ -7,4 +7,4 @@ ldf=/home/nico/Dokumente/Workspaces/semantic-iot/linked-data-fu-0/bin/ldfu.sh
 #curl -f -X PUT -T ontology/ibm-vocab.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 3
 #curl -f -X PUT -T ontology/list-vocab.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 4
 
-$ldf -p ontology/rules/data-retrieval-ldpc.n3 -p ontology/rules/ibm-semantics.n3 -p ontology/rules/list-semantics.n3 -p ontology/rules/selected_ontology_semantics.n3 -p ontology/rules/spin_sparql_ask_where_query_processing.n3 -p workflow/rules/lightCheck.n3 -o - #-n 100 # arguments
+$ldf -p ontology/rules/data-retrieval-ldpc.n3 -p ontology/rules/ibm-semantics.n3 -p ontology/rules/list-semantics.n3 -p ontology/rules/selected_ontology_semantics.n3 -p ontology/rules/spin_sparql_ask_where_query_processing.n3 -p workflow/rules/lightCheck.n3 -n 100 # arguments
