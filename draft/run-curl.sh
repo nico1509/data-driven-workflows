b35d1e9244e0bc25225065675f317694103460c1
Nico Assfalg <nico.assfalg@student.kit.edu>
2019-01-11 15:55:23 +0100

    Evacuation Workflow

--- draft/run-curl.sh
+++ draft/run-curl.sh
@@ -3,6 +3,6 @@
 ldf=/home/nico/Dokumente/Workspaces/semantic-iot/linked-data-fu-0/bin/ldfu.sh
 
 curl -f -X DELETE http://tok450s.lan:8080/ldbbc/ || exit 1
-curl -f -X PUT -T workflow/ibm-workflow.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 2
+curl -f -X PUT -T workflow/evacuation-workflow.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 2
 curl -f -X PUT -T ontology/ibm-vocab.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 3
 curl -f -X PUT -T ontology/list-vocab.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 4
