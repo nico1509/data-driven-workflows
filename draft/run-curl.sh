e7bb21f6a71282ccb7c40471d68cf7175ef6feb2
Nico Assfalg <nico.assfalg@student.kit.edu>
2019-01-26 20:37:09 +0100

    Include WF Initialization in run-curl.sh

--- draft/run-curl.sh
+++ draft/run-curl.sh
@@ -6,3 +6,4 @@ curl -f -X DELETE http://tok450s.lan:8080/ldbbc/ || exit 1
 curl -f -X PUT -T workflow/evacuation-workflow.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 2
 curl -f -X PUT -T ontology/ibm-vocab.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 3
 curl -f -X PUT -T ontology/list-vocab.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 4
+curl -f -X PUT -T workflow/evacuation-workflow-instance.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 5
