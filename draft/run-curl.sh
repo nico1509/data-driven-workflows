1533199a0fcdc4fe7a982201fb555ed91fb8d890
Nico Assfalg <nico.assfalg@student.kit.edu>
2019-01-29 16:02:02 +0100

    Add mock handshake to run-curl.sh

--- draft/run-curl.sh
+++ draft/run-curl.sh
@@ -2,6 +2,7 @@
 
 ldf=/home/nico/Dokumente/Workspaces/semantic-iot/linked-data-fu-0/bin/ldfu.sh
 
+curl http://mock-server.lan:3000/iphs || exit 6
 curl -f -X DELETE http://tok450s.lan:8080/ldbbc/ || exit 1
 curl -f -X PUT -T workflow/evacuation-workflow.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 2
 curl -f -X PUT -T ontology/ibm-vocab.ttl http://tok450s.lan:8080/ldbbc/ -Hcontent-type:text/turtle || exit 3
